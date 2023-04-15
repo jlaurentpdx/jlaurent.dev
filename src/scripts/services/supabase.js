d38615258e4c17b8e1259baa03c8106c4466666c
Jordan Laurent <jlaurent.pdx@gmail.com>
2023-04-14 17:23:23 -0700

    logging projects to console

---
+++ src/scripts/services/supabase.js
@@ -0,0 +1,15 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
+const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+const parseData = ({ data, error }) => {
+  if (error) throw error
+  return data
+}
+
+export async function getProjects() {
+  const projects = await supabase.from('projects').select('*')
+  return parseData(projects)
+}
