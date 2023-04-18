987d2f6749c546494956d47dc4f110fe5573c7ce
Jordan Laurent <jlaurent.pdx@gmail.com>
2023-04-17 23:14:24 -0700

    added link astro component and link data sheet

---
+++ src/data/links.js
@@ -0,0 +1,34 @@
+export const links = {
+  linkedIn: {
+    link: 'https://www.linkedin.com/in/jlaurentpdx/',
+    text: 'LinkedIn',
+  },
+  github: {
+    link: 'https://github.com/jlaurentpdx',
+    text: 'GitHub',
+  },
+  email: {
+    link: 'mailto:jlaurent.pdx@gmail.com',
+    text: 'jlaurent.pdx@gmail.com',
+  },
+  processing: {
+    link: 'https://openprocessing.org/browse/#',
+    text: 'creative coding',
+  },
+  mutualAid: {
+    link: 'https://www.instagram.com/crisiskitchenpdx/',
+    text: 'mutual aid',
+  },
+  volunteering: {
+    link: 'https://rahabs-sisters.org/',
+    text: 'volunteering',
+  },
+  workshops: {
+    link: 'https://www.instagram.com/p/CCcdmKXhMnT/',
+    text: 'workshops',
+  },
+  mentoring: {
+    link: 'https://francescafr.github.io/PCC2018/2018/',
+    text: 'mentoring',
+  },
+}
