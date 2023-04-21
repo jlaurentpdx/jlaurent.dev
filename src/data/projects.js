94c63a368851eb96727ff4b5081319e152ece939
Jordan Laurent <jlaurent.pdx@gmail.com>
2023-04-20 23:38:52 -0700

    replace react project components with astro

---
+++ src/data/projects.js
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    title: 'Geo Tone',
+    label: 'geo-tone',
+    description:
+      'Geo Tone is an audio sequencer made for the browser with user creativity and accessibility in mind. Built with React 18, Tone.js, and Framer Motion on the front-end and a custom back-end with Express and Node.',
+    imageUrl: '/gifs/geotone.gif',
+    deployUrl: 'https://geo-tone.netlify.app/',
+    repoUrl: 'https://github.com/geo-tone/geo-tone-frontend',
+  },
+  {
+    title: 'friends.map()',
+    label: 'friends-dot-map',
+    description:
+      'friends (dot) map is a convenient travel companion for keeping small groups and travel parties up-to-date with real-time status updates and geolocation. Uses React 17, Mapbox, Tailwind CSS, and Supabase with WebSockets.',
+    imageUrl: '/gifs/friends.gif',
+    deployUrl: 'https://friends-dot-map.netlify.app/',
+    repoUrl: 'https://github.com/friends-dot-map/friends-dot-map',
+  },
+  {
+    title: 'Scrounger',
+    label: 'scrounger',
+    description:
+      'Portland, Oregon has many sights to see, and Scrounger puts them in the palm of your hand. Choose an adventure, follow the hints provided, and check out that view! Made with React 17, Supabase, and a love of exploration.',
+    imageUrl: '/gifs/scrounger.gif',
+    deployUrl: 'https://scrounger.netlify.app/',
+    repoUrl: 'https://github.com/scavengers-club/scrounger',
+  },
+  {
+    title: 'Sprout',
+    label: 'sprout',
+    description:
+      'Sprout makes it easy to be a proud new plant parent. Browse a list of beginner-friendly houseplants, then build your budding relationship with our plant-specific caretaking guide. Made mobile-first, desktop-responsive with JavaScript, CSS, and HTML.',
+    imageUrl: '/gifs/sprout.gif',
+    deployUrl: 'https://alchemy-plant-app.github.io/plant-app/',
+    repoUrl: 'https://github.com/plant-pardy/sprout',
+  },
+]
