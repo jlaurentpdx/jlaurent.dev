f2a8bd2f8bbb9dc28d11d522d973f6064f023ddf
Jordan Laurent <jlaurent.pdx@gmail.com>
2023-04-25 15:38:24 -0700

    replaced Link component, updated imports

---
+++ src/data/contactLinks.js
@@ -0,0 +1,17 @@
+export const contactLinks = {
+  linkedIn: {
+    label: 'Jordan on LinkedIn',
+    link: 'https://www.linkedin.com/in/jlaurentpdx/',
+    text: 'LinkedIn',
+  },
+  github: {
+    label: 'Jordan on GitHub',
+    link: 'https://github.com/jlaurentpdx',
+    text: 'GitHub',
+  },
+  email: {
+    label: 'Email Jordan',
+    link: 'mailto:jlaurent.pdx@gmail.com',
+    text: 'jlaurent.pdx@gmail.com',
+  },
+}
