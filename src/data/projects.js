0e604ea3c298fa12e01f426e5ea89cbdf2e23b15
Jordan Laurent <jlaurent.pdx@gmail.com>
2023-04-25 17:14:45 -0700

    improved link handling in projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,5 @@
+import { icons } from './icons.js'
+
 export const projects = [
   {
     title: 'Geo Tone',
@@ -5,26 +7,26 @@ export const projects = [
     description:
       'Geo Tone is an audio sequencer made for the browser with user creativity and accessibility in mind. Built with React 18, Tone.js, and Framer Motion on the front-end and a custom back-end with Express and Node.',
     imageUrl: '/gifs/geotone.gif',
-    deployUrl: 'https://geo-tone.netlify.app/',
-    repoUrl: 'https://github.com/geo-tone/geo-tone-frontend',
-  },
-  {
-    title: 'friends.map()',
-    label: 'friends-dot-map',
-    description:
-      'friends (dot) map is a convenient travel companion for keeping small groups and travel parties up-to-date with real-time status updates and geolocation. Uses React 17, Mapbox, Tailwind CSS, and Supabase with WebSockets.',
-    imageUrl: '/gifs/friends.gif',
-    deployUrl: 'https://friends-dot-map.netlify.app/',
-    repoUrl: 'https://github.com/friends-dot-map/friends-dot-map',
-  },
-  {
-    title: 'Scrounger',
-    label: 'scrounger',
-    description:
-      'Portland, Oregon has many sights to see, and Scrounger puts them in the palm of your hand. Choose an adventure, follow the hints provided, and check out that view! Made with React 17, Supabase, and a love of exploration.',
-    imageUrl: '/gifs/scrounger.gif',
-    deployUrl: 'https://scrounger.netlify.app/',
-    repoUrl: 'https://github.com/scavengers-club/scrounger',
+    links: [
+      {
+        content: 'Live Deploy',
+        url: 'https://geo-tone.netlify.app/',
+        src: 'netlify',
+        icon: icons.netlify,
+      },
+      {
+        content: 'Frontend Repo',
+        url: 'https://github.com/geo-tone/geo-tone-frontend',
+        src: 'github',
+        icon: icons.github,
+      },
+      {
+        content: 'Backend Repo',
+        url: 'https://github.com/geo-tone/geo-tone-backend',
+        src: 'github',
+        icon: icons.github,
+      },
+    ],
   },
   {
     title: 'Sprout',
@@ -32,7 +34,19 @@ export const projects = [
     description:
       'Sprout makes it easy to be a proud new plant parent. Browse a list of beginner-friendly houseplants, then build your budding relationship with our plant-specific caretaking guide. Made mobile-first, desktop-responsive with JavaScript, CSS, and HTML.',
     imageUrl: '/gifs/sprout.gif',
-    deployUrl: 'https://alchemy-plant-app.github.io/plant-app/',
-    repoUrl: 'https://github.com/plant-pardy/sprout',
+    links: [
+      {
+        content: 'Live Deploy',
+        url: 'https://alchemy-plant-app.github.io/plant-app/',
+        src: 'github',
+        icon: icons.githubPages,
+      },
+      {
+        content: 'Frontend Repo',
+        url: 'https://github.com/plant-pardy/sprout',
+        src: 'github',
+        icon: icons.github,
+      },
+    ],
   },
 ]
