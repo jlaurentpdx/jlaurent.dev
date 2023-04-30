dc38d962a8905028ccc363247313daa7f3a60073
Jordan Laurent <jlaurent.pdx@gmail.com>
2023-04-29 22:57:43 -0700

    update data files

---
+++ src/data/mediaLinks.js
@@ -0,0 +1,12 @@
+export const mediaLinks = {
+  collabLab: {
+    label: 'Visit The Collab Lab',
+    link: 'https://the-collab-lab.codes/',
+    text: 'The Collab Lab',
+  },
+  techTalk: {
+    label: "View Jordan's tech talk on YouTube",
+    link: 'https://www.youtube.com/watch?v=u76CKtRjgq0',
+    text: '2022 tech talk',
+  },
+}
--- src/data/techStack.js
+++ src/data/techStack.js
@@ -1,1 +1,1 @@
-export const techStack = ['React', 'ES6', 'Express', 'Node', 'PostgreSQL', 'Supabase', 'Tone.js', 'and Mapbox']
+export const techStack = ['JavaScript', 'React', 'Express', 'Node', 'PostgreSQL', 'Supabase', 'Tone.js', 'and Mapbox']
