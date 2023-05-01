dc2cb8feb2ed29009ff26674af0b0a5fdedff2d3
Jordan Laurent <jlaurent.pdx@gmail.com>
2023-04-30 19:48:40 -0700

    improve project formatting on project page

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,8 +5,10 @@ export const projects = [
     title: 'Geo Tone',
     label: 'geo-tone',
     description:
-      'Geo Tone is an audio sequencer made for the browser with user creativity and accessibility in mind. Built with React 18, Tone.js, and Framer Motion on the front-end and a custom back-end with Express and Node.',
+      '<strong>Geo Tone</strong> is an audio sequencer made for the browser with user creativity and accessibility in mind. Built with React 18, Tone.js, and Framer Motion on the front-end and a custom back-end with Express and Node.',
     imageUrl: '/gifs/geotone.gif',
+    imageAlt:
+      'A musical sequencer with vibrant colors is used by a performer, changing properties of the sequencer like which notes and instruments are being played and at what volume.',
     links: [
       {
         content: 'Live Deploy',
@@ -32,8 +34,10 @@ export const projects = [
     title: 'Sprout',
     label: 'sprout',
     description:
-      'Sprout makes it easy to be a proud new plant parent. Browse a list of beginner-friendly houseplants, then build your budding relationship with our plant-specific caretaking guide. Made mobile-first, desktop-responsive with JavaScript, CSS, and HTML.',
+      '<strong>Sprout</strong> makes it easy to be a proud new plant parent. Browse a list of beginner-friendly houseplants, then build your budding relationship with our plant-specific caretaking guide. Made mobile-first, desktop-responsive with JavaScript, CSS, and HTML.',
     imageUrl: '/gifs/sprout.gif',
+    imageAlt:
+      'A mobile phone displays a user selecting houseplants from a list of beginner-friendly options, such as the Peace Lily and String of Hearts. The user navigates to a page that shows important caretaking information for their selected plants.',
     links: [
       {
         content: 'Live Deploy',
@@ -53,8 +57,10 @@ export const projects = [
     title: 'Beginning Maker Tech',
     label: 'beginning-maker-tech',
     description:
-      'Taught from January to March 2019, Beginning Maker Techniques and Technologies introduced students to programming and microelectronics with Arduino, CNC laser cutting, and graphic design with Adobe Illustrator.',
+      'Taught from January to March 2019, <strong>Beginning Maker Techniques and Technologies</strong> introduced students to programming and microelectronics with Arduino, CNC laser cutting, and graphic design with Adobe Illustrator.',
     imageUrl: '/images/bmtech.jpg',
+    imageAlt:
+      'A web page displays a lesson about understanding code, featuring a distinction between high-level and low-level programming languages, a simple chart on what binary digits can be used represent, and a connection to solving complex problems with simple logic.',
     links: [
       {
         content: 'Archived Course',
