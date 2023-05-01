deecc8df9b0c71ccc482804a79002fec82177688
Jordan Laurent <jlaurent.pdx@gmail.com>
2023-05-01 00:25:10 -0700

    add netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
-import preact from '@astrojs/preact'
+import netlify from '@astrojs/netlify/functions'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  output: 'server',
+  adapter: netlify(),
 })
