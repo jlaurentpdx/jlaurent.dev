d3ef2255a67149a62a5f7f45f0fee6c06342b661
Jordan Laurent <jlaurent.pdx@gmail.com>
2023-04-30 21:52:12 -0700

    update to general styling

--- src/data/icons.ts
+++ src/data/icons.ts
@@ -1,3 +1,5 @@
+// References libraries and icons found through Iconify (https://icon-sets.iconify.design/) as used by astro-icon
+
 export const icons = {
   netlify: 'simple-icons:netlify',
   github: 'ion:logo-github',
