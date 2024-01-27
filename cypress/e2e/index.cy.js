4d783fc3677b5a66171b068d796ab89aef36d1f8
jlaurentpdx <jlaurent.pdx@gmail.com>
2024-01-26 20:06:34 -0800

    removes unused dependencies

--- cypress/e2e/index.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Home Page', () => {
-  it('title and heading are correct', () => {
-    const page = cy.visit('/')
-
-    page.get('title').should('have.text', 'Jordan Laurent | Home')
-    page.get('h1').should('have.text', "Howdy. I'm Jordan")
-  })
-})
